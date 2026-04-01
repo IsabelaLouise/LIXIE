2c81cd826a0b838a4dddaa58859bb58b7dea3e63
Isabela Louise <isabelalouise.cs@gmail.com>
2026-04-01 15:59:04 -0300

    add sql

---
+++ cadastrese.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# 🔌 conexão com banco
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ilcs050607",
+    database="Lixie"
+)
+
+cursor = conexao.cursor()
+
+# 🚀 ROTA DE CADASTRO
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    dados = request.json
+
+    try:
+        sql = """
+        INSERT INTO Usuario 
+        (Nome, Email, Senha, Data_Nasc, Endereco, Numero, CEP, Telefone, Dt_Criacao, Pontuacao_Total_Acumulada_, Nivel)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, CURDATE(), 0, 1)
+        """
+
+        valores = (
+            dados["nome"],
+            dados["email"],
+            dados["senha"],
+            dados["data_nasc"],
+            dados["rua"],
+            dados["numeroCasa"],
+            dados["cep"],
+            dados["telefone"]
+        )
+
+        cursor.execute(sql, valores)
+        conexao.commit()
+
+        return jsonify({"mensagem": "Cadastrado com sucesso!"})
+
+    except Exception as e:
+        return jsonify({"erro": str(e)})
+
+# 🚀 iniciar servidor
+app.run(debug=True)
