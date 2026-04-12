a0a18f71656dfbac4e7f04a69fb8ed6ceaded042
Isabela Louise <isabelalouise.cs@gmail.com>
2026-04-12 19:07:00 -0300

    tentando vercel

--- .vscode/src/cadastrese.py
+++ .vscode/src/cadastrese.py
@@ -22,9 +22,9 @@ class ServidorCadastro(http.server.BaseHTTPRequestHandler):
     
     # === CORS ===
     def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*') # Permite qualquer site (Vercel) acessar
-        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         super().end_headers()
 
     def do_OPTIONS(self):
