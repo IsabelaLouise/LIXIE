bf1f26ef9195b8884ea9531701f8594170c291b0
Isabela Louise <isabelalouise.cs@gmail.com>
2026-04-12 20:50:23 -0300

    corrigindo erro com servidor do login

--- .vscode/src/login.js
+++ .vscode/src/login.js
@@ -6,7 +6,7 @@ document.getElementById("formLogin").addEventListener("submit", async function(e
   const mensagem = document.getElementById("mensagem");
 
   try {
-    const resposta = await fetch("http://localhost:8000/login", {
+    const resposta = await fetch("https://lixie-production.up.railway.app/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
