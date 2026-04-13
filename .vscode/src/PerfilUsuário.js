1e1c0959646d3b9c996490c104f6975bff99db80
Ju-P0 <portela.julia@pucpr.edu.br>
2026-04-12 22:43:25 -0300

    update de imagem funcionando

---
+++ .vscode/src/PerfilUsuário.js
@@ -0,0 +1,7 @@
+let fotoPerfil= document.getElementById('avatar');
+let inputFoto = document.getElementById('foto-perfil');
+
+inputFoto.onchange =  function(){
+    fotoPerfil.src = URL.createObjectURL(inputFoto.files[0]);
+}
+
